c54c01bd88dc5ecde7b0dbc8fbd892eba39874ab
antiquark <141497727+antiquark007@users.noreply.github.com>
2024-12-18 19:32:14 +0530

    added solution 1

--- Sorting and Searching/DistinctNumbers.cpp
+++ Sorting and Searching/DistinctNumbers.cpp
@@ -9,6 +9,7 @@ using namespace std;
 #define isEven(x) ((x % 2 == 0) ? 1 : 0)
 #define sumAll(v) accumulate(v.begin(), v.end(), 0)
 #define SORT(v) sort(v.begin(), v.end())
+
 int32_t main()
 {
     ios::sync_with_stdio(0);
@@ -17,12 +18,24 @@ int32_t main()
 
     int n;
     cin >> n;
-    unordered_map<int, int> hm;
-    for(int i=0;i<n;i++){
-        int x;
-        cin >> x;
-        hm[x]++;
-    }
-    cout<<hm.size();
+    vi v(n);
+    read(v, n);
 
+    SORT(v);
+
+    int ans = 1;
+    for (int i = 1; i < n; i++) {
+        if (v[i] != v[i - 1]) {
+            ans++;
+        }
+    }
+    // unordered_map<int, int> hm;
+    // for(int i=0;i<n;i++){
+    //     int x;
+    //     cin >> x;
+    //     hm[x]++;
+    // }
+    // cout<<hm.size();
+
+    cout << ans;
 }
